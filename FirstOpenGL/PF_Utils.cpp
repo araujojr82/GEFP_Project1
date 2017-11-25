9b9dc557622a8004e160646897d7cec4765c5ee3
Jorge <jorge.amengol@gmail.com>
2017-11-25 12:19:14 -0400

    File update

--- FirstOpenGL/PF_Utils.cpp
+++ FirstOpenGL/PF_Utils.cpp
@@ -6,10 +6,12 @@ void loadObjectsUsingFactory(cFactory* pFactory, std::vector< iGameObject* > vec
     pSpaceShip->setName("PlayerShip");
     pSpaceShip->SetVelocity(glm::vec3(0.0f, 0.0f, 0.1f));
     vecObjects.push_back(pSpaceShip);
-    iGameObject* pSpaceShip2 = pFactory->CreateObject("fighter2");
-    pSpaceShip->setName("PlayerShip2");
-    pSpaceShip->SetVelocity(glm::vec3(0.0f, 0.0f, 0.1f));
-    vecObjects.push_back(pSpaceShip2);
+    
+    // These lines create a new object just by recompiling this object. Uncoment to test!
+    //iGameObject* pSpaceShip2 = pFactory->CreateObject("fighter2");
+    //pSpaceShip->setName("PlayerShip2");
+    //pSpaceShip->SetVelocity(glm::vec3(0.0f, 0.0f, 0.1f));
+    //vecObjects.push_back(pSpaceShip2);
 
     for (int index = 0; index != 1; index++)
     {
